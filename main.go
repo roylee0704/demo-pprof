17c05de9a5c7cae89119477d79a790da1e8993f6
roylee0704 <roylee0704@gmail.com>
2017-05-06 22:41:41 +0800

    Add simple httpserver

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var visitors int
+
+func handleHi(w http.ResponseWriter, r *http.Request) {
+	if match, _ := regexp.MatchString(`^\w*$`, r.FormValue("color")); !match {
+		http.Error(w, "Optional color is invalid", http.StatusBadRequest)
+		return
+	}
+	visitors++
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf(`
+        <h1 style='color:%s'>Welcome!</h1>
+        You are visitor number %d!
+    `, r.FormValue("color"), visitors)))
+}
+
+func main() {
+	log.Print("Starting on port 8080")
+	http.HandleFunc("/hi", handleHi)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
